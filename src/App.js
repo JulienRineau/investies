de9bc07088b69e013b067772b759f6943933fca2
Julien Rineau <julien.rineau@berkeley.edu>
2023-11-05 18:05:36 -0800

    make it better

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   Button,
   Card,
   Spinner,
+  Image,
 } from "react-bootstrap";
 import {
   Chart as ChartJS,
@@ -20,6 +21,8 @@ import {
   Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
+import appleLogo from "./apple.png";
+import teslaLogo from "./tesla.png";
 
 ChartJS.register(
   CategoryScale,
@@ -32,8 +35,9 @@ ChartJS.register(
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
-  const [data, setData] = useState(null);
+  const [dataList, setDataList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [companyImage, setCompanyImage] = useState(null);
 
   const fetchData = async (query) => {
     if (!query) {
@@ -47,8 +51,19 @@ function App() {
       `http://127.0.0.1:5000/net-incomes?search_input=${query}`
     );
     const result = await response.json();
-    setData(result);
+    setDataList((prevDataList) => [result, ...prevDataList]);
     setIsLoading(false);
+
+    // Set company image based on the search input
+    const appleRegex = /apple/i;
+    const teslaRegex = /tesla/i;
+    if (appleRegex.test(query)) {
+      setCompanyImage(appleLogo);
+    } else if (teslaRegex.test(query)) {
+      setCompanyImage(teslaLogo);
+    } else {
+      setCompanyImage(null);
+    }
   };
 
   const handleSearchInputChange = (event) => {
@@ -60,11 +75,15 @@ function App() {
     fetchData(text);
   };
 
+  const handleCardClose = (index) => {
+    setDataList((prevDataList) => prevDataList.filter((_, i) => i !== index));
+  };
+
   return (
     <div className="app-background">
       <Container>
         <Row className="justify-content-md-center">
-          <Col md="auto">
+          <Col md="auto" style={{ maxWidth: "800px" }}>
             <h1>Investies</h1>
             <p>Empowering smarter investment decisions with AI</p>
             <InputGroup>
@@ -97,7 +116,7 @@ function App() {
                 <Button
                   variant="outline-light"
                   className="w-100"
-                  onClick={() => handleButtonClick("What is Tesla sales")}
+                  onClick={() => handleButtonClick("What are Apple sales")}
                 >
                   What is Tesla sales
                 </Button>
@@ -107,7 +126,9 @@ function App() {
                   variant="outline-light"
                   className="w-100"
                   onClick={() =>
-                    handleButtonClick("Compare Apple and Tesla gross margin")
+                    handleButtonClick(
+                      "Compare Apple gross margin to the industry"
+                    )
                   }
                 >
                   Compare Apple and Tesla gross margin
@@ -118,13 +139,22 @@ function App() {
                   variant="outline-light"
                   className="w-100"
                   onClick={() =>
-                    handleButtonClick("What are the risks at Tesla")
+                    handleButtonClick("What are the risks at Apple")
                   }
                 >
                   What are the risks at Tesla
                 </Button>
               </Col>
             </Row>
+            {companyImage && (
+              <Image
+                src={companyImage}
+                alt="Company logo"
+                fluid
+                className="mt-3"
+                style={{ maxWidth: "775px" }}
+              />
+            )}
             {isLoading && (
               <div className="mt-3">
                 <Spinner animation="border" role="status">
@@ -132,52 +162,76 @@ function App() {
                 </Spinner>
               </div>
             )}
-            {!isLoading && data && (
-              <Card className="mt-3">
-                <Card.Body>
-                  {/* Display user's question */}
-                  <Card.Title>{data.search_input}</Card.Title>
+            {!isLoading &&
+              dataList.map((data, index) => (
+                <Card key={index} className="mt-3">
+                  <Card.Body>
+                    <div className="d-flex justify-content-between">
+                      {/* Display user's question */}
+                      <Card.Title>{data.search_input}</Card.Title>
 
-                  {/* Display AI's answer */}
-                  <Card.Text
-                    style={{ fontSize: "1rem", whiteSpace: "pre-wrap" }}
-                  >
-                    {data.answer}
-                  </Card.Text>
-
-                  {/* Display graph if available */}
-                  {data.chart && (
-                    <Bar
-                      data={data.chart.data}
-                      options={{
-                        scales: {
-                          y: {
-                            beginAtZero: true,
-                          },
-                        },
-                      }}
-                    />
-                  )}
-
-                  {/* Display sources */}
-                  <Card.Text className="mt-3">Sources:</Card.Text>
-                  {Object.keys(data.sources).map((key, index) => {
-                    const yearMatch = data.sources[key].Source.match(/\d{4}/);
-                    const year = yearMatch ? yearMatch[0] : "";
-                    return (
+                      {/* Close button */}
                       <Button
-                        key={key}
-                        className="mr-2 mb-2"
-                        variant="outline-success"
-                        title={`Source: ${data.sources[key].Source}\nPage: ${data.sources[key].Page}`}
+                        variant="outline-danger"
+                        size="sm"
+                        onClick={() => handleCardClose(index)}
                       >
-                        {year} - Page {data.sources[key].Page}
+                        X
                       </Button>
-                    );
-                  })}
-                </Card.Body>
-              </Card>
-            )}
+                    </div>
+
+                    {/* Display AI's answer */}
+                    <Card.Text
+                      style={{ fontSize: "1rem", whiteSpace: "pre-wrap" }}
+                    >
+                      {data.answer}
+                    </Card.Text>
+
+                    {/* Display graph if available */}
+                    {data.chart && (
+                      <Bar
+                        data={data.chart.data}
+                        options={{
+                          scales: {
+                            y: {
+                              beginAtZero: true,
+                            },
+                          },
+                          plugins: {
+                            legend: {
+                              labels: {
+                                color: "rgba(103, 173, 91, 0.6)", // Set the legend text color to match the background
+                              },
+                            },
+                          },
+                          elements: {
+                            bar: {
+                              backgroundColor: "rgba(103, 173, 91, 0.6)", // Set the bar color to match the background
+                            },
+                          },
+                        }}
+                      />
+                    )}
+
+                    {/* Display sources */}
+                    <Card.Text className="mt-3">Sources:</Card.Text>
+                    {Object.keys(data.sources).map((key, index) => {
+                      const yearMatch = data.sources[key].Source.match(/\d{4}/);
+                      const year = yearMatch ? yearMatch[0] : "";
+                      return (
+                        <Button
+                          key={key}
+                          className="mr-2 mb-2"
+                          variant="outline-success"
+                          title={`Source: ${data.sources[key].Source}\nPage: ${data.sources[key].Page}`}
+                        >
+                          {year} - Page {data.sources[key].Page}
+                        </Button>
+                      );
+                    })}
+                  </Card.Body>
+                </Card>
+              ))}
           </Col>
         </Row>
       </Container>
--- src/CompanyAnalysis.js
+++ src/CompanyAnalysis.js
@@ -1,71 +1,189 @@
+import React, { useState } from "react";
+import "./index.css";
 import {
   Container,
   Row,
   Col,
   InputGroup,
   FormControl,
+  Button,
   Card,
-  ProgressBar,
+  Spinner,
 } from "react-bootstrap";
-import "./CompanyAnalysis.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-function CompanyAnalysis() {
-  const sections = [
-    {
-      title: "Revenue Growth",
-      chart: "Graph Data Placeholder",
-      explanation:
-        "Revenue is growing rapidly, indicating a strong business performance.",
-    },
-    {
-      title: "Profit Margin",
-      chart: "Graph Data Placeholder",
-      explanation:
-        "Profit margin is low, which may signify operational inefficiencies.",
-    },
-  ];
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [dataList, setDataList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = async (query) => {
+    if (!query) {
+      alert("Please enter a search query.");
+      return;
+    }
+
+    setIsLoading(true);
+
+    const response = await fetch(
+      `http://127.0.0.1:5000/net-incomes?search_input=${query}`
+    );
+    const result = await response.json();
+    setDataList((prevDataList) => [result, ...prevDataList]);
+    setIsLoading(false);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleButtonClick = (text) => {
+    setSearchInput(text);
+    fetchData(text);
+  };
 
   return (
-    <Container className="company-analysis">
-      <Row>
-        <Col>
-          <h1>APPLE.INC</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <InputGroup>
-            <FormControl
-              type="search"
-              placeholder="Search financial document..."
-              aria-label="Search"
-            />
-          </InputGroup>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col sm={4} md={3} className="text-center">
-          <div className="confidence-score">
-            <h4>Investment Confidence</h4>
-            <ProgressBar variant="success" now={75} label="75" srOnly />
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        {sections.map((section, index) => (
-          <Col key={index} md={6}>
-            <Card className="section">
-              <Card.Header>{section.title}</Card.Header>
-              <Card.Body>
-                <Card.Text>{section.chart}</Card.Text>
-                <Card.Text>{section.explanation}</Card.Text>
-              </Card.Body>
-            </Card>
+    <div className="app-background">
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <h1>Investies</h1>
+            <p>Empowering smarter investment decisions with AI</p>
+            <InputGroup>
+              <FormControl
+                type="search"
+                placeholder="Search..."
+                aria-label="Search"
+                value={searchInput}
+                onChange={handleSearchInputChange}
+              />
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={() => fetchData(searchInput)}
+              >
+                Submit
+              </Button>
+            </InputGroup>
+            <Row className="mt-3 justify-content-around">
+              <Col xs={12} sm={6} className="mb-2">
+                <Button
+                  variant="outline-light"
+                  className="w-100"
+                  onClick={() => handleButtonClick("Make a report on Apple")}
+                >
+                  Make a report on Apple
+                </Button>
+              </Col>
+              <Col xs={12} sm={6} className="mb-2">
+                <Button
+                  variant="outline-light"
+                  className="w-100"
+                  onClick={() => handleButtonClick("What is Tesla sales")}
+                >
+                  What is Tesla sales
+                </Button>
+              </Col>
+              <Col xs={12} sm={6} className="mb-2">
+                <Button
+                  variant="outline-light"
+                  className="w-100"
+                  onClick={() =>
+                    handleButtonClick("Compare Apple and Tesla gross margin")
+                  }
+                >
+                  Compare Apple and Tesla gross margin
+                </Button>
+              </Col>
+              <Col xs={12} sm={6} className="mb-2">
+                <Button
+                  variant="outline-light"
+                  className="w-100"
+                  onClick={() =>
+                    handleButtonClick("What are the risks at Tesla")
+                  }
+                >
+                  What are the risks at Tesla
+                </Button>
+              </Col>
+            </Row>
+            {isLoading && (
+              <div className="mt-3">
+                <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+              </div>
+            )}
+            {!isLoading &&
+              dataList.map((data, index) => (
+                <Card key={index} className="mt-3">
+                  <Card.Body>
+                    {/* Display user's question */}
+                    <Card.Title>{data.search_input}</Card.Title>
+
+                    {/* Display AI's answer */}
+                    <Card.Text
+                      style={{ fontSize: "1rem", whiteSpace: "pre-wrap" }}
+                    >
+                      {data.answer}
+                    </Card.Text>
+
+                    {/* Display graph if available */}
+                    {data.chart && (
+                      <Bar
+                        data={data.chart.data}
+                        options={{
+                          scales: {
+                            y: {
+                              beginAtZero: true,
+                            },
+                          },
+                        }}
+                      />
+                    )}
+
+                    {/* Display sources */}
+                    <Card.Text className="mt-3">Sources:</Card.Text>
+                    {Object.keys(data.sources).map((key, index) => {
+                      const yearMatch = data.sources[key].Source.match(/\d{4}/);
+                      const year = yearMatch ? yearMatch[0] : "";
+                      return (
+                        <Button
+                          key={key}
+                          className="mr-2 mb-2"
+                          variant="outline-success"
+                          title={`Source: ${data.sources[key].Source}\nPage: ${data.sources[key].Page}`}
+                        >
+                          {year} - Page {data.sources[key].Page}
+                        </Button>
+                      );
+                    })}
+                  </Card.Body>
+                </Card>
+              ))}
           </Col>
-        ))}
-      </Row>
-    </Container>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
-export default CompanyAnalysis;
+export default App;
